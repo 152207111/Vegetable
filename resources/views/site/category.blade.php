3d798c0c999a0ec161ef4b9101ce957dc0bf3444
Ipson Gurung <nsomething221@gmail.com>
2023-06-07 18:49:54 +0545

    category+responsive+images

--- resources/views/site/category.blade.php
+++ resources/views/site/category.blade.php
@@ -110,7 +110,7 @@
         <img src="{{ asset('site/image/vegetable.jpg')}}" alt="">
       </div>
       <div class="image-title">
-        <h2><a href="">Vegetable</a></h2>
+        <h2><a href="#vegetable">Vegetable</a></h2>
       </div>
     </div>
     <div class="category-item">
@@ -118,7 +118,7 @@
         <img src="{{ asset('site/image/fruit.jpg')}}" alt="">
       </div>
       <div class="image-title">
-        <h2><a href="">Fruit</a></h2>
+        <h2><a href="#fruit">Fruit</a></h2>
       </div>
     </div>
 
@@ -127,7 +127,7 @@
         <img src="{{ asset('site/image/spices.jpg')}}" alt="">
       </div>
       <div class="image-title">
-        <h2><a href="">Spices</a></h2>
+        <h2><a href="#spices">Spices</a></h2>
       </div>
     </div>
 
@@ -136,7 +136,7 @@
         <img src="{{ asset('site/image/other.jpg')}}" alt="">
       </div>
       <div class="image-title">
-        <h2><a href="">Other items</a></h2>
+        <h2><a href="#other-items">Other items</a></h2>
       </div>
     </div>
   </div>
@@ -144,7 +144,7 @@
 
 
  <section>
-  <div class="product-section">
+  <div class="product-section" id="vegetable">
     <div class="title" style="text-align:center;">
       <h2><span>Vege</span>table</h2>
     </div>
@@ -203,10 +203,100 @@
           </div>
         </div>
       </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+          Capsicum
+          </div>
+          <img src="{{ asset('site/image/	Capsicum.webp')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.8<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.60</span>|<del>Rs.80</del>|</h1>
+          <h3>Category: Vegetable</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+             Tomato
+          </div>
+          <img src="{{ asset('site/image/tomato.png')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.9<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.75</span>|<del>Rs.100</del>|</h1>
+          <h3>Category: Vegetable</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+             Cauliflower
+          </div>
+          <img src="{{ asset('site/image/cauliflower.jpg')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.5<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.100</span>|<del>Rs.120</del>|</h1>
+          <h3>Category: Vegetable</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Cabbage
+          </div>
+          <img src="{{ asset('site/image/cabbage.jpg ')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.8<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.80</span>|<del>Rs.110</del>|</h1>
+          <h3>Category: Vegetable</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Carrot
+          </div>
+          <img src="{{ asset('site/image/carrot.jpg')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.9<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.120</span>|<del>Rs.150</del>|</h1>
+          <h3>Category: Vegetable</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
-  <div class="product-section">
+  <div class="product-section" id="fruit">
     <div class="title" style="text-align:center;">
       <h2><span>Fru</span>it</h2>
     </div>
@@ -265,10 +355,100 @@
           </div>
         </div>
       </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Grape
+          </div>
+          <img src="{{ asset('site/image/Grapes.webp')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.7<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.150</span>|<del>RS.180</del>|</h1>
+          <h3>Category: Fruit</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+          Banana
+          </div>
+          <img src="{{ asset('site/image/Banana.jpg')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.9<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.170</span>|<del>RS.200</del>|</h1>
+          <h3>Category: Fruit</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+          Pineapple
+          </div>
+          <img src="{{ asset('site/image/Pineapple.jpg')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.6<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.150</span>|<del>RS.170</del>|</h1>
+          <h3>Category: Fruit</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Mango
+          </div>
+          <img src="{{ asset('site/image/Mango.webp')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.7<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.180</span>|<del>RS.200</del>|</h1>
+          <h3>Category: Fruit</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Watermelon
+          </div>
+          <img src="{{ asset('site/image/Watermelon.jpg')}}" alt="">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.5<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.150</span>|<del>RS.180</del>|</h1>
+          <h3>Category: Fruit</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
-  <div class="product-section">
+  <div class="product-section" id="spices">
     <div class="title" style="text-align:center;">
       <h2><span>Spi</span>ces</h2>
     </div>
@@ -327,12 +507,248 @@
           </div>
         </div>
       </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Coriander
+          </div>
+          <img src="{{ asset('site/image/Coriander.webp')}}" alt="Cumin image">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.5<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.50</span>|<del>RS.70</del>|</h1>
+          <h3>Category: Spices</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Garlic
+          </div>
+          <img src="{{ asset('site/image/Garlic.webp')}}" alt="Cumin image">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.5<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.60</span>|<del>RS.80</del>|</h1>
+          <h3>Category: Spices</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Methi
+          </div>
+          <img src="{{ asset('site/image/Methi.jpg')}}" alt="Cumin image">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.3<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.80</span>|<del>RS.100</del>|</h1>
+          <h3>Category: Spices</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Dalachini
+          </div>
+          <img src="{{ asset('site/image/Dalachini.png')}}" alt="Cumin image">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.4<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.100</span>|<del>RS.120</del>|</h1>
+          <h3>Category: Spices</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
+      <div class="product">
+        <div class="image">
+          <div class="product-title">
+            Jaiphal
+          </div>
+          <img src="{{ asset('site/image/Jaiphal.jpg')}}" alt="Cumin image">
+        </div>
+        <div class="content">
+          <div class="product-top">
+            <h2>100% Fresh <span class="rate">4.2<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+          </div>
+          <h1><span>Rs.80</span>|<del>RS.100</del>|</h1>
+          <h3>Category: Spices</h3>
+          <div class="button">
+            <button>View Detail</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
-  <div class="product-section">
+  <div class="product-section" id="other-items">
     <div class="title" style="text-align:center;">
       <h2><span>Other</span>-items</h2>
+    </div>
+    <div class="product-detail">
+      <div class="product">
+          <div class="image">
+            <div class="product-title">
+              Cashew
+            </div>
+            <img src="{{ asset('site/image/Kaju.jpg')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.4<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.200</span>|<del>RS.220</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="image">
+            <div class="product-title">
+              Almond
+            </div>
+            <img src="{{ asset('site/image/Almond.webp')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.6<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.180</span>|<del>RS.200</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="image">
+            <div class="product-title">
+              Peanut
+            </div>
+            <img src="{{ asset('site/image/Peanut.png')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.7<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.150</span>|<del>RS.180</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="image">
+            <div class="product-title">
+              Walnut
+            </div>
+            <img src="{{ asset('site/image/Walnut.jpg')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.5<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.160</span>|<del>RS.180</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="image">
+            <div class="product-title">
+              Coconut
+            </div>
+            <img src="{{ asset('site/image/Coconut.jpg')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.4<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.210</span>|<del>RS.250</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="image">
+            <div class="product-title">
+             Pistachio
+            </div>
+            <img src="{{ asset('site/image/Pistachio.webp')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.4<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.140</span>|<del>RS.160</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="image">
+            <div class="product-title">
+               Apricots
+            </div>
+            <img src="{{ asset('site/image/Apricots.jpg')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.5<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.250</span>|<del>RS.300</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="image">
+            <div class="product-title">
+             Raisins
+            </div>
+            <img src="{{ asset('site/image/Raisins.webp')}}" alt="Cumin image">
+          </div>
+          <div class="content">
+            <div class="product-top">
+              <h2>100% Fresh <span class="rate">4.8<span class="star"><i class="fa-solid fa-star"></i></span></span></h2>
+            </div>
+            <h1><span>Rs.300</span>|<del>RS.350</del>|</h1>
+            <h3>Category: Other-items</h3>
+            <div class="button">
+              <button>View Detail</button>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
 </section>
